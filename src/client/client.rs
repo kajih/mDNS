32f5fe581e36b133bd941e86acde0a0b13ad6e9c
kajih <kajih@gramr>
2026-04-23 20:39:04 +0200

    Fixed the client

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,37 +1,32 @@
 use mdns_sd::{ServiceDaemon, ServiceEvent};
-use std::io;
-use std::io::Write;
+use std::time::Duration;
 
 fn main() -> std::io::Result<()> {
-    // Create a daemon
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
-    // Browse for a service type.
     let service_type = "_boxbiter._tcp.local.";
-
     let receiver = mdns.browse(service_type).expect("Failed to browse");
+    let timeout = Duration::from_secs(10);
 
-    // Receive the browse events in sync or async. Here is
-    // an example of using a thread. Users can call `receiver.recv_async().await`
-    // if running in async environment.
-    std::thread::spawn(move || {
-        while let Ok(event) = receiver.recv() {
-            match event {
-                ServiceEvent::ServiceResolved(resolved) => {
-                    println!("Resolved a new service: {}", resolved.fullname);
-                }
-                other_event => {
-                    println!("Received other event: {:?}", &other_event);
-                }
+    loop {
+        match receiver.recv_timeout(timeout) {
+            Ok(ServiceEvent::ServiceResolved(info)) => {
+                println!("Resolved: {}", info.get_fullname());
+                println!("  Hostname : {}", info.get_hostname());
+                println!("  Addresses: {:?}", info.get_addresses());
+                println!("  Port     : {}", info.get_port());
+                println!("  Properties: {:?}", info.get_properties());
+                break;
+            }
+            Ok(other_event) => {
+                println!("Received other event: {:?}", other_event);
+            }
+            Err(_) => {
+                println!("Timed out after {}s waiting for service.", timeout.as_secs());
+                break;
             }
         }
-    });
-
-    // Gracefully shutdown the daemon.
-    let mut input = String::new();
-    print!("Press Enter to continue...");
-    io::stdout().flush()?; // make sure prompt appears before waiting
-    io::stdin().read_line(&mut input)?;
+    }
 
     mdns.shutdown().unwrap();
 
